9cff7f9fe8c688c1ef279b552346edcc45019497
henryadebayo <henryadebayo250@gmail.com>
2022-01-11 14:03:46 +0100

    omoo

--- lib/services/repository.dart
+++ lib/services/repository.dart
@@ -4,10 +4,20 @@ import 'package:http/http.dart' as http;
  const Url ='https://sportive-23.herokuapp.com/api/v1/auth/signup';
 
 void postData() async{
-  http.Response response = await http.post(Uri.parse(Url),
-    headers: <String, String>{
-    'Content-Type':'application/json',
-    },
-    body: josnEncode(<>{})
-  );
+  try {
+    http.Response response = await http.post(Uri.parse(Url),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: {
+          "firstName": "John",
+          "lastName": "Snow",
+          "email": "johnsow@email.com",
+          "password": "john123"
+        }
+    );
+    print(response.statusCode);
+  }catch(e){
+    print(e);
+  }
 }
